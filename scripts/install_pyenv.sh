5ca98a7285802b2e5eaf99f0bda76d77821c0b80
Jacob Neil Taylor <jacobneiltaylor@canva.com>
2023-09-08 13:16:56 +1000

    Remove md command from pyenv script

--- scripts/install_pyenv.sh
+++ scripts/install_pyenv.sh
@@ -2,8 +2,6 @@
 export PYENV_VERSION=${1:-"2.3.26"}
 export PYENV_ROOT=${2:-"/opt/pyenv-$PYENV_VERSION"}
 
-mkdir -p $PYENV_ROOT
-
 curl -L --proto '=https' --tlsv1.2 "https://github.com/pyenv/pyenv/archive/refs/tags/v$PYENV_VERSION.tar.gz" | tar -C /tmp -zxf -
 mv /tmp/pyenv* $PYENV_ROOT
 
